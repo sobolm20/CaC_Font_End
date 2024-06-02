4f8b4675c5c6609efbfa2a500542c54a22409a8c
sobolm20 <matiassobol20@gmail.com>
2024-06-02 00:57:09 -0300

    version-3

--- js/script.js
+++ js/script.js
@@ -1,23 +1,45 @@
-const popup = document.getElementById("popup");
-const name = document.getElementById("name");
+const showHiddenPass = (inputPass, inputIcon) => {
+    const input = document.getElementById(inputPass),
+        iconEye = document.getElementById(inputIcon)
+
+    iconEye.addEventListener('click', () =>{
+        if (input.type === 'password') {
+            input.type = 'text'
+            iconEye.classList.add('ri-eye-line')
+            iconEye.classList.remove('ri-eye-off-line')
+        } else {
+            input.type = 'password'
+            iconEye.classList.remove('ri-eye-line')
+            iconEye.classList.add('ri-eye-off-line')
+        }
+    })
+}
+showHiddenPass('input-pass', 'input-icon')
+
 
 function Register (){
-const form = document.getElementById("formy");
-const dates = form.querySelectorAll('input');
-
-const items = {};
-for (let date of dates){
-    if(date.value !==''){
-        items[date.id] = date.value;
+    const form = document.getElementById("formy");
+    const dates = form.querySelectorAll('input-email', 'input-user', 'input-pass');
+    
+    const items = {};
+    for (let date of dates){
+        if(date.value !==''){
+            items[date.id] = date.value;
+        }
     }
-}
-localStorage.setItem('Register', JSON.stringify(items));
-}
-
-if(name) {
-    popup.classList.add("open-popup")
-}
-
-const closePopup = ()=> {
-    popup.classList.remove("open-popup")
-} 
+    localStorage.setItem('registered', JSON.stringify(items));
+    }
+    
+    button.addEventListener('DOMContentLoaded', function () {
+        let memory = localStorage.getItem('registered');
+        if(memory){
+            let forma = document.getElementById("formy");
+            let obj = JSON.parse(memory);
+    
+            for(let date of obj){
+                let element = forma.querySelector('#${date}');
+                element.value = obj[date];
+            }
+        }
+    });
+    
